ab692a502b1786548648a08b1080248c62110190
Lucas Lima <lucasarts2020@gmail.com>
2025-08-04 22:49:05 -0300

    lifting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 
 function App() {
+  const [useInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
   return (
     <>
       <Header />
       <UserInput />
+      <Results />
     </>
   );
 }
